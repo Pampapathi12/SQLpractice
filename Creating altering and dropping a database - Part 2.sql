bad5dc018e63f71088ceef4db3953121f646c289
Pampapathi K <pampapathi@kalajitha.com>
2024-10-04 21:07:29 +0530

    Creating altering and dropping a database - Part 2

--- Creating altering and dropping a database - Part 2.sql
+++ Creating altering and dropping a database - Part 2.sql
@@ -17,3 +17,22 @@ Create Database Pampapathi1
 
 // store procedure is group of sql commands to execute
 // 2 types store procedure . system store procedure and user defined store procedure
+
+// alter database pampapathi1 use store procedure
+
+sp_renameDB 'pampapathi1', 'pampapathi2'
+
+drop the data base,  
+
+Drop database pampapathi2
+// when database is use another use then the if delete then it will not delete 
+
+
+// deleting or dropping a database
+
+Alter Database Pampapathi2 Set SINGLE_USER With Rollback Immediate
+Drop database pampapathi2
+
+
+
+
