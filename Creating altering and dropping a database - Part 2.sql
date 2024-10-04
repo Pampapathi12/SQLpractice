0112903cdda6ec8465b70f09bacbfa142340ac3f
Pampapathi K <pampapathi@kalajitha.com>
2024-10-04 20:54:25 +0530

    Creating altering and dropping a database - Part 2

---
+++ Creating altering and dropping a database - Part 2.sql
@@ -0,0 +1,19 @@
+//database is collection tables and objects
+
+// Creating and altering database 1
+
+// create database using query
+
+Create Database Pampapathi1 
+
+// know the mdf and ldf file C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA
+
+// mdf master data file mdf
+// transaction log files ldf :- two files will create
+
+// rename database
+
+1. Alter Database pampapathi Modify Name = Pampa
+
+// store procedure is group of sql commands to execute
+// 2 types store procedure . system store procedure and user defined store procedure
